c4dfe7ea0da3abdd82cc2caea5a25592a090179f
rikard-sics <rikard.hoglund@ri.se>
2019-09-03 14:51:26 +0200

    Continued implementation of OSCORE observe server and client

--- demo-apps/cf-oscore-observe-client/src/main/java/org/eclipse/californium/examples/OSCOREObserveClient.java
+++ demo-apps/cf-oscore-observe-client/src/main/java/org/eclipse/californium/examples/OSCOREObserveClient.java
@@ -26,11 +26,21 @@ import java.net.URISyntaxException;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.elements.exception.ConnectorException;
+import org.eclipse.californium.elements.util.Bytes;
+import org.eclipse.californium.oscore.HashMapCtxDB;
+import org.eclipse.californium.oscore.OSCoreCoapStackFactory;
+import org.eclipse.californium.oscore.OSCoreCtx;
+import org.eclipse.californium.oscore.OSException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.network.config.NetworkConfigDefaultHandler;
 import org.eclipse.californium.core.network.config.NetworkConfig.Keys;
-
+import org.eclipse.californium.cose.AlgorithmID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OSCOREObserveClient {
 
@@ -48,7 +58,21 @@ public class OSCOREObserveClient {
 			config.setInt(Keys.PREFERRED_BLOCK_SIZE, DEFAULT_BLOCK_SIZE);
 		}
 	};
-
+	
+	//OSCORE context information for client
+	private final static HashMapCtxDB dbClient = new HashMapCtxDB();
+	private final static AlgorithmID alg = AlgorithmID.AES_CCM_16_64_128;
+	private final static AlgorithmID kdf = AlgorithmID.HKDF_HMAC_SHA_256;
+	private final static byte[] master_secret = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
+			0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+	private final static byte[] master_salt = { (byte) 0x9e, (byte) 0x7c, (byte) 0xa9, (byte) 0x22, (byte) 0x23,
+			(byte) 0x78, (byte) 0x63, (byte) 0x40 };
+	private final static byte[] context_id = { 0x11, 0x22, 0x33, 0x44 };
+	private final static byte[] rid = { (byte) 0xAA };
+	private final static byte[] sid = { (byte) 0xCC };
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(OSCOREObserveClient.class.getCanonicalName());
+	
 	/*
 	 * Application entry point.
 	 * 
@@ -65,17 +89,31 @@ public class OSCOREObserveClient {
 			try {
 				uri = new URI(args[0]);
 			} catch (URISyntaxException e) {
-				System.err.println("Invalid URI: " + e.getMessage());
+				LOGGER.error("Invalid URI: " + e.getMessage());
 				System.exit(-1);
 			}
 			
-			CoapClient client = new CoapClient(uri);
-
+			//Add OSCORE context for the client
+			try {
+				OSCoreCtx ctx = new OSCoreCtx(master_secret, false, alg, sid, rid, kdf, 32, master_salt, context_id);
+				dbClient.addContext(uri.toString(), ctx);
+			}
+			catch (OSException e) {
+				LOGGER.error("Failed to set server OSCORE Context information!");
+			}
+			OSCoreCoapStackFactory.useAsDefault(dbClient);
+			
+			//Create and send OSCORE request
+			CoapClient client = new CoapClient();
+			Request request = Request.newGet();
+			request.setURI(uri);
+			request.getOptions().setOscore(Bytes.EMPTY);
+			
 			CoapResponse response = null;
 			try {
-				response = client.get();
+				response = client.advanced(request);
 			} catch (ConnectorException | IOException e) {
-				System.err.println("Got an error: " + e);
+				LOGGER.error("Got an error: " + e);
 			}
 
 			if (response!=null) {
@@ -105,9 +143,8 @@ public class OSCOREObserveClient {
 			System.out.println("Californium (Cf) OSCORE observe Client");
 			System.out.println("(c) 2014, Institute for Pervasive Computing, ETH Zurich");
 			System.out.println();
-			System.out.println("Usage : " + OSCOREObserveClient.class.getSimpleName() + " URI [file]");
-			System.out.println("  URI : The CoAP URI of the remote resource to GET");
-			System.out.println("  file: optional filename to save the received payload");
+			System.out.println("Usage : " + OSCOREObserveClient.class.getSimpleName() + " URI");
+			System.out.println("  URI : The CoAP URI of the remote resource to observe");
 		}
 	}
 
--- demo-apps/cf-oscore-observe-server/src/main/java/org/eclipse/californium/examples/OSCOREObserveServer.java
+++ demo-apps/cf-oscore-observe-server/src/main/java/org/eclipse/californium/examples/OSCOREObserveServer.java
@@ -28,37 +28,59 @@ import org.eclipse.californium.core.network.CoapEndpoint;
 import org.eclipse.californium.core.network.EndpointManager;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.eclipse.californium.cose.AlgorithmID;
 import org.eclipse.californium.elements.tcp.TcpServerConnector;
+import org.eclipse.californium.oscore.HashMapCtxDB;
+import org.eclipse.californium.oscore.OSCoreCoapStackFactory;
+import org.eclipse.californium.oscore.OSCoreCtx;
+import org.eclipse.californium.oscore.OSException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OSCOREObserveServer extends CoapServer {
 
 	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
-	private static final int TCP_THREADS = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.TCP_WORKER_THREADS);
-	private static final int TCP_IDLE_TIMEOUT = NetworkConfig.getStandard()
-			.getInt(NetworkConfig.Keys.TCP_CONNECTION_IDLE_TIMEOUT);
+	
+	//OSCORE context information for server
+	private final static HashMapCtxDB dbServer = new HashMapCtxDB();
+	private final static AlgorithmID alg = AlgorithmID.AES_CCM_16_64_128;
+	private final static AlgorithmID kdf = AlgorithmID.HKDF_HMAC_SHA_256;
+	private final static byte[] master_secret = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
+			0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+	private final static byte[] master_salt = { (byte) 0x9e, (byte) 0x7c, (byte) 0xa9, (byte) 0x22, (byte) 0x23,
+			(byte) 0x78, (byte) 0x63, (byte) 0x40 };
+	private final static byte[] context_id = { 0x11, 0x22, 0x33, 0x44 };
+	private final static byte[] rid = { (byte) 0xCC };
+	private final static byte[] sid = { (byte) 0xAA };
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(OSCOREObserveServer.class.getCanonicalName());
+	
 	/*
 	 * Application entry point.
 	 */
 	public static void main(String[] args) {
 
+		//Add OSCORE context for the server
+		try {
+			OSCoreCtx ctx = new OSCoreCtx(master_secret, false, alg, sid, rid, kdf, 32, master_salt, context_id);
+			dbServer.addContext(ctx);
+		}
+		catch (OSException e) {
+			LOGGER.error("Failed to set server OSCORE Context information!");
+		}
+		OSCoreCoapStackFactory.useAsDefault(dbServer);
+		
 		try {
 			// create server
 			boolean udp = true;
-			boolean tcp = false;
-			if (0 < args.length) {
-				tcp = args[0].equalsIgnoreCase("coap+tcp:");
-				if (tcp) {
-					System.out.println("Please Note: the TCP support is currently experimental!");
-				}
-			}
+
 			OSCOREObserveServer server = new OSCOREObserveServer();
 			// add endpoints on all IP addresses
-			server.addEndpoints(udp, tcp);
+			server.addEndpoints(udp);
 			server.start();
 
 		} catch (SocketException e) {
-			System.err.println("Failed to initialize server: " + e.getMessage());
+			LOGGER.error("Failed to initialize server: " + e.getMessage());
 		}
 	}
 
@@ -66,20 +88,13 @@ public class OSCOREObserveServer extends CoapServer {
 	 * Add individual endpoints listening on default CoAP port on all IPv4
 	 * addresses of all network interfaces.
 	 */
-	private void addEndpoints(boolean udp, boolean tcp) {
+	private void addEndpoints(boolean udp) {
 		NetworkConfig config = NetworkConfig.getStandard();
 		for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
 			InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
 			if (udp) {
 				CoapEndpoint.Builder builder = new CoapEndpoint.Builder();
 				builder.setInetSocketAddress(bindToAddress);
-				builder.setNetworkConfig(config);
-				addEndpoint(builder.build());
-			}
-			if (tcp) {
-				TcpServerConnector connector = new TcpServerConnector(bindToAddress, TCP_THREADS, TCP_IDLE_TIMEOUT);
-				CoapEndpoint.Builder builder = new CoapEndpoint.Builder();
-				builder.setConnector(connector);
 				builder.setNetworkConfig(config);
 				addEndpoint(builder.build());
 			}
@@ -94,7 +109,9 @@ public class OSCOREObserveServer extends CoapServer {
 	public OSCOREObserveServer() throws SocketException {
 
 		// provide an instance of a Hello-World resource
-		add(new HelloWorldResource());
+		HelloWorldResource helloWorld = new HelloWorldResource();
+		LOGGER.info("Added new resource " + helloWorld.getURI());
+		add(helloWorld);
 	}
 
 	/*
