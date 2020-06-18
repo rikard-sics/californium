a10eea43dc9be52b55de49293823b0078c1f28f3
rikard-sics <rikard.hoglund@ri.se>
2020-06-18 09:38:35 +0200

    Initial commit of Group OSCORE code

--- cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupOSCORESender.java
+++ cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupOSCORESender.java
@@ -46,16 +46,6 @@ import org.eclipse.californium.elements.config.Configuration.DefinitionsProvider
 
 /**
  * Test sender configured to support multicast requests. Rebased.
-=======
-import com.upokecenter.cbor.CBORObject;
-
-import net.i2p.crypto.eddsa.EdDSASecurityProvider;
-
-import org.eclipse.californium.core.network.config.NetworkConfigDefaultHandler;
-
-/**
- * Test sender configured to support multicast requests.
->>>>>>> 6cf51e7a0 (Initial commit of Group OSCORE code)
  */
 public class GroupOSCORESender {
 
@@ -144,7 +134,6 @@ public class GroupOSCORESender {
 			"A501781A636F6170733A2F2F6D79736974652E6578616D706C652E636F6D026C67726F75706D616E6167657203781A636F6170733A2F2F646F6D61696E2E6578616D706C652E6F7267041AAB9B154F08A101A4010103272006215820CDE3EFD3BC3F99C9C9EE210415C6CBA55061B5046E963B8A58C9143A61166472");
 
 	private final static byte[] sid = new byte[] { 0x25 };
-
 	private final static byte[] sid_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
 			"A501781B636F6170733A2F2F746573746572312E6578616D706C652E636F6D02666D796E616D6503781A636F6170733A2F2F68656C6C6F312E6578616D706C652E6F7267041A70004B4F08A101A4010103272006215820069E912B83963ACC5941B63546867DEC106E5B9051F2EE14F3BC5CC961ACD43A");
 	private static MultiKey sid_private_key;
