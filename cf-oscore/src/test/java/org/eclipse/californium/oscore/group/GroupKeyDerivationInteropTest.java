4bf49e70b66306d9e5e1367b91dee4a3b2a0cf0f
rikard-sics <rikard.hoglund@ri.se>
2020-07-04 17:23:33 +0200

    Begun adding method for decoding a COSE OneKey from diagnostic notation

--- cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupKeyDerivationInteropTest.java
+++ cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupKeyDerivationInteropTest.java
@@ -181,13 +181,6 @@ public class GroupKeyDerivationInteropTest {
 
 	@Test
 	public void testSenderKeys() throws OSException {
-		try {
-			parseDiagnosticOneKey("a");
-		} catch (CoseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 		// Check that sender keys match in both contexts
 		assertArrayEquals(senderCtxEcdsa.getSenderKey(), senderCtxEddsa.getSenderKey());
 
@@ -306,6 +299,15 @@ public class GroupKeyDerivationInteropTest {
 				sharedSecret2);
 	}
 
+	@Test
+	public void testDiagnosticKeyDecoding() {
+
+			parseDiagnosticOneKey(
+					"{1: 1, -1: 6, -2: h’4C5E5A898AFC77D9C90773D9B4F5E7B378605753F9BA9E8A62488C64E1A524B0’, -4: h’C9AFCF6610BAB69A7E72B78B6D364BE86F12CF293523DA51433B09A799FF0F62’}");
+
+
+	}
+
 	/* --- End of tests --- */
 
 	/**
@@ -371,10 +373,111 @@ public class GroupKeyDerivationInteropTest {
 
 	}
 
-	private void parseDiagnosticOneKey(String keyString) throws CoseException {
-		OneKey test = OneKey.generateKey(AlgorithmID.EDDSA);
-		System.out.println(test.AsCBOR().ToJSONString());
-		CBORObject test2;
+	/**
+	 * Parse a string representing a COSE OneKey in diagnostic notation. This
+	 * method first converts it to a JSON string and then decodes it to a CBOR
+	 * Object using built in methods. A COSE OneKey is then created from that
+	 * CBOR Object.
+	 * 
+	 * @param keyString string representing a OneKey in diagnostic notation
+	 * @return a OneKey object built from the string
+	 */
+	private OneKey parseDiagnosticOneKey(String keyString) {
+		// OneKey test = OneKey.generateKey(AlgorithmID.EDDSA);
+		// System.out.println(test.AsCBOR().ToJSONString());
+		// CBORObject test2;
+		// CBORObject test3 =
+		// CBORObject.FromJSONString(test.AsCBOR().ToJSONString());
+		// System.out.println(test3.ToJSONString());
+
+		// Convert to lower case
+		keyString = keyString.toLowerCase();
+
+		// Remove { and } characters
+		keyString = keyString.replace("{", "");
+		keyString = keyString.replace("}", "");
+		// Remove spaces
+		keyString = keyString.replace(" ", "");
+
+		// Split the string into sections at the , and : character
+		String[] segments = keyString.split("[,:]");
+
+		// Change every even element to have quotes around it
+		for (int i = 0; i < segments.length; i += 2) {
+			segments[i] = "\"" + segments[i] + "\"";
+		}
+
+		// Convert byte arrays to Base64
+		for (int i = 0; i < segments.length; i++) {
+
+			if (segments[i].length() >= 2 && segments[i].substring(0, 2).equals("h’")) {
+				// Remove h’ and ’
+				String arrayString = segments[i].replace("h’", "").replace("’", "");
+
+				// Convert to base64
+				byte[] array = Utils.hexToBytes(arrayString);
+				String arrayBase64 = DatatypeConverter.printBase64Binary(array);
+
+				// Change it to base64url encoding
+				arrayBase64 = arrayBase64.replace("+", "-");
+				arrayBase64 = arrayBase64.replace("/", "_");
+
+				// Remove padding
+				arrayBase64 = arrayBase64.replace("=", "");
+
+				segments[i] = "\"" + arrayBase64 + "\"";
+			}
+		}
+
+		// Reassemble everything into a string
+		StringBuilder jsonString = new StringBuilder();
+		jsonString.append("{");
+		for (int i = 0; i < segments.length; i++) {
+			jsonString.append(segments[i]);
+
+			if (i % 2 == 0) {
+				jsonString.append(":");
+			} else if (i % 2 != 0 && i != segments.length - 1) {
+				jsonString.append(",");
+			}
+		}
+		jsonString.append("}");
+
+		// Parse the JSON string into a CBOR Object
+		CBORObject keyCbor = CBORObject.FromJSONString(jsonString.toString());
+		System.out.println("TPYE" + keyCbor.getType());
+		// Set the key type if missing (which it sometimes is)
+		if (keyCbor.get(KeyKeys.KeyType.AsCBOR()) == null) {
+			// Checks and sets the key type for ECDSA
+			CBORObject ec2Curve = keyCbor.get(KeyKeys.EC2_Curve.AsCBOR());
+
+			System.out.println("ec2Curve" + ec2Curve);
+
+			if (ec2Curve == KeyKeys.EC2_P256 || ec2Curve == KeyKeys.EC2_P384 || ec2Curve == KeyKeys.EC2_P521) {
+				System.out.println("HELLo1");
+				keyCbor.set(KeyKeys.KeyType.AsCBOR(), KeyKeys.KeyType_EC2);
+			}
+
+			// Checks and sets the key type for EDDSA
+			CBORObject okpCurve = keyCbor.get(KeyKeys.OKP_Curve.AsCBOR());
+			if (okpCurve == KeyKeys.OKP_Ed25519) {
+				System.out.println("HELLo2");
+				keyCbor.set(KeyKeys.KeyType.AsCBOR(), KeyKeys.KeyType_OKP);
+			}
+		}
+
+		System.out.println("WWWW " + keyCbor.ToJSONString());
+
+		// Create a COSE key from CBOR Object
+		OneKey key = null;
+		try {
+			key = new OneKey(keyCbor);
+		} catch (CoseException e) {
+			System.err.println("Error: Failed to decode COSE OneKey from diagnostic notation.");
+			e.printStackTrace();
+		}
+
+		return key;
 	}
 
 }
