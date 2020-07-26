4a5b87660b1ba6a9c65f6dd606527e9d1cc227f0
rikard-sics <rikard.hoglund@ri.se>
2020-07-26 20:01:40 +0200

    Fix to functionality for disabling replay checks

--- cf-oscore/src/main/java/org/eclipse/californium/oscore/Decryptor.java
+++ cf-oscore/src/main/java/org/eclipse/californium/oscore/Decryptor.java
@@ -212,6 +212,12 @@ public abstract class Decryptor {
 				
 		}
 
+		System.out.println("Decrypting incoming " + message.getClass().getSimpleName());
+		System.out.println("Key " + Utils.toHexString(ctx.getRecipientKey()));
+		System.out.println("PartialIV " + Utils.toHexString(partialIV));
+		System.out.println("Nonce " + Utils.toHexString(nonce));
+		System.out.println("AAD " + Utils.toHexString(aad));
+
 		byte[] plaintext = null;
 		byte[] key = ctx.getRecipientKey();
 		byte[] detRecipientKey = null;
