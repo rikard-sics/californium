c79867b9fd6abf2404a468b50e9b78feaddc7240
marco-tiloca-sics <marco.tiloca@ri.se>
2020-12-02 18:30:32 +0100

    Signatures with COSE Sign1

--- cf-edhoc/src/test/java/org/eclipse/californium/edhoc/EdhocClient.java
+++ cf-edhoc/src/test/java/org/eclipse/californium/edhoc/EdhocClient.java
@@ -23,6 +23,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.Provider;
+import java.security.Security;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
@@ -31,9 +33,16 @@ import org.eclipse.californium.core.coap.CoAP.Code;
 import org.eclipse.californium.core.coap.CoAP.Type;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.elements.exception.ConnectorException;
+
+import net.i2p.crypto.eddsa.EdDSASecurityProvider;
+
 import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.network.config.NetworkConfigDefaultHandler;
 import org.eclipse.californium.core.network.config.NetworkConfig.Keys;
+import org.eclipse.californium.cose.AlgorithmID;
+import org.eclipse.californium.cose.CoseException;
+import org.eclipse.californium.cose.KeyKeys;
+import org.eclipse.californium.cose.OneKey;
 
 public class EdhocClient {
 
@@ -42,7 +51,18 @@ public class EdhocClient {
 	private static final int DEFAULT_MAX_RESOURCE_SIZE = 2 * 1024 * 1024; // 2
 																			// MB
 	private static final int DEFAULT_BLOCK_SIZE = 512;
-
+	
+	private final static Provider EdDSA = new EdDSASecurityProvider();
+	
+	// Uncomment to set ECDSA with curve P-256 for signatures
+    private final static int signKeyCurve = KeyKeys.EC2_P256.AsInt32();
+    
+    // Uncomment to set EDDSA with curve Ed25519 for signatures
+    // private final static int signKeyCurve = KeyKeys.OKP_Ed25519.AsInt32();
+    
+    // The long-term asymmetric key pair of this peer
+	private static OneKey keyPair = null;
+	
 	private static NetworkConfigDefaultHandler DEFAULTS = new NetworkConfigDefaultHandler() {
 
 		@Override
@@ -64,6 +84,20 @@ public class EdhocClient {
 		NetworkConfig config = NetworkConfig.createWithFile(CONFIG_FILE, CONFIG_HEADER, DEFAULTS);
 		NetworkConfig.setStandard(config);
 
+		// Generate the new long-term asymmetric key pair 
+		try {
+	 		if (signKeyCurve == KeyKeys.EC2_P256.AsInt32())
+	 			keyPair = OneKey.generateKey(AlgorithmID.ECDSA_256);
+	    	if (signKeyCurve == KeyKeys.OKP_Ed25519.AsInt32()) {
+	        	Security.insertProviderAt(EdDSA, 0);
+	    		keyPair = OneKey.generateKey(AlgorithmID.EDDSA);
+	    	}
+			
+		} catch (CoseException e) {
+			System.err.println("Error while generating the key pair");
+			return;
+		}
+		
 		URI uri = null; // URI parameter of the request
 
 		// input URI from command line arguments
