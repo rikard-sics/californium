619f6a349b5cc91d70010dbb3ff93d3ff0cc2f55
rikard-sics <rikard.hoglund@ri.se>
2020-12-08 10:59:53 +0100

    JUnit test for method to build a CBOR sequence

--- cf-edhoc/src/test/java/org/eclipse/californium/edhoc/UtilTest.java
+++ cf-edhoc/src/test/java/org/eclipse/californium/edhoc/UtilTest.java
@@ -1,9 +1,17 @@
 package org.eclipse.californium.edhoc;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.upokecenter.cbor.CBORObject;
 
 import net.i2p.crypto.eddsa.Utils;
 
@@ -40,6 +48,38 @@ public class UtilTest {
 				"ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f");
 
 		Assert.assertArrayEquals(expected, hash);
+	}
+
+	/**
+	 * Test building a CBOR sequence and then parsing it with built in CBOR
+	 * methods.
+	 * 
+	 * @throws IOException on test failure
+	 * 
+	 */
+	@Test
+	public void testBuildCBORSequence() throws IOException {
+		// Build a list of CBOR objects with 4 elements
+		List<CBORObject> objectListIn = new ArrayList<CBORObject>();
+		objectListIn.add(CBORObject.FromObject(true));
+		objectListIn.add(CBORObject.FromObject(100));
+		objectListIn.add(CBORObject.FromObject(new byte[] { 0x01 }));
+		objectListIn.add(CBORObject.FromObject("hello"));
+
+		// Create the bytes of the sequence
+		byte[] sequence = Util.buildCBORSequence(objectListIn);
+
+		// Parse the sequence bytes with CBOR
+		InputStream sequenceStream = new ByteArrayInputStream(sequence);
+		CBORObject[] objectArrayOut = CBORObject.ReadSequence(sequenceStream);
+		List<CBORObject> objectListOut = Arrays.asList(objectArrayOut);
+		// objectListOut.set(1, CBORObject.FromObject(200));
+
+		// Compare the result with the original input
+		Assert.assertEquals(objectListIn.size(), objectListOut.size());
+		for (int i = 0; i < objectListIn.size(); i++) {
+			Assert.assertTrue(objectListOut.contains(objectListIn.get(i)));
+		}
 	}
 
 }
