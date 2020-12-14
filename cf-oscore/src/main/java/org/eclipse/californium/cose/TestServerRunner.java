e485ed31f87c41e6ee0bb88642e019f964aa9716
Rikard Höglund <rikard.hoglund@ri.se>
2020-12-14 09:37:06 +0100

    Fixing indentation

--- cf-oscore/src/main/java/org/eclipse/californium/cose/TestServerRunner.java
+++ cf-oscore/src/main/java/org/eclipse/californium/cose/TestServerRunner.java
@@ -8,8 +8,8 @@ import net.i2p.crypto.eddsa.EdDSASecurityProvider;
 public class TestServerRunner {
     public static void main(String[] args) throws Exception {
         int a = 1;
-		Provider EdDSAX = new EdDSASecurityProvider();
-		Security.insertProviderAt(EdDSAX, 1); // Must be 1!
+        Provider EdDSAX = new EdDSASecurityProvider();
+        Security.insertProviderAt(EdDSAX, 1); // Must be 1!
 
         org.eclipse.californium.cose.Tester.runme();
     }
