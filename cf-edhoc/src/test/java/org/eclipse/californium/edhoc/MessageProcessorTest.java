d8e171fe7018d013ca09873cb4b48dfcae6fb8fc
marco-tiloca-sics <marco.tiloca@ri.se>
2021-02-22 09:34:18 +0100

    Aligned test vectors to v -05 of the draft

--- cf-edhoc/src/test/java/org/eclipse/californium/edhoc/MessageProcessorTest.java
+++ cf-edhoc/src/test/java/org/eclipse/californium/edhoc/MessageProcessorTest.java
@@ -33,15 +33,14 @@ public class MessageProcessorTest {
 	 * Tests identification of EDHOC messages. Based on messages from the EDHOC test vectors.
 	 * 
 	 */
-	@Ignore
 	@Test
 	public void testMessageType() {
 		byte[] message1 = Utils
-				.hexToBytes("4001005820898ff79a02067a16ea1eccb90fa52246f5aa4dd6ec076bba0259d904b7ec8b0c2e");
+				.hexToBytes("01005820898ff79a02067a16ea1eccb90fa52246f5aa4dd6ec076bba0259d904b7ec8b0c2e");
 		byte[] message2 = Utils.hexToBytes(
-				"582071a3d599c21da18902a1aea810b2b6382ccd8d5f9bf0195281754c5ebcaf301e375850334097283a57820696782ba2be46c2ec2e02ecd55d88fe43084fedf97c9bb8122580e3f145c4632d5b677a25bf836a1d2209f15cba9ecfb267412e225983db18fe4f80b1be1d6508299cf88da9bbe9f3");
+				"582071a3d599c21da18902a1aea810b2b6382ccd8d5f9bf0195281754c5ebcaf301e3758500ff2ac2d7e87ae340e50bbde9f70e8a77f86bf659f43b024a73ee97b6a2b9c5592fd835a15178b7c28af5474a9758148647d3d98a8731e164c9c70528107f40f21463ba811bf039719e7cffaa7f2f440");
 		byte[] message3 = Utils.hexToBytes(
-				"3758581c6682d608d6adce9f82a3eb13438dc7db43aa7ef0b1acc362fe6f74d8c39b31defb9d6e9e3afb24a47f9b8be72122637d922a43a1d7770b8719be1a517b75bef965812c10f7d7cdc663bdaa09c5c9fa342feb07a3c245fa");
+				"375858f5f6debd8214051cd583c84096c4801debf35b15363dd16ebd8530dfdcfb34fcd2eb6cad1dac66a479fb38deaaf1d30a7e6817a22ab04f3d5b1e972a0d13ea86c66b60514c9657ea89c57b0401edc5aa8bbcab813cc5d6e7");
 
 		Assert.assertEquals(Constants.EDHOC_MESSAGE_1, MessageProcessor.messageType(message1));
 		Assert.assertEquals(Constants.EDHOC_MESSAGE_2, MessageProcessor.messageType(message2));
@@ -66,7 +65,6 @@ public class MessageProcessorTest {
 	 * 
 	 * See: https://tools.ietf.org/html/draft-ietf-lake-edhoc-05#appendix-B.1.1
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage1B1() {
 		// First set up the session to use
@@ -96,7 +94,7 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 
 		byte[] expectedMessage1 = Utils
-				.hexToBytes("4001005820898ff79a02067a16ea1eccb90fa52246f5aa4dd6ec076bba0259d904b7ec8b0c2e");
+				.hexToBytes("01005820898ff79a02067a16ea1eccb90fa52246f5aa4dd6ec076bba0259d904b7ec8b0c2e");
 		
 		Assert.assertArrayEquals(expectedMessage1, message1);
 	}
@@ -106,7 +104,6 @@ public class MessageProcessorTest {
 	 * 
 	 * See: https://tools.ietf.org/html/draft-ietf-lake-edhoc-05#appendix-B.2.1
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage1B2() {
 		// First set up the session to use
@@ -137,7 +134,7 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 		
 		byte[] expectedMessage1 = Utils
-				.hexToBytes("400d0058208d3ef56d1b750a4351d68ac250a0e883790efc80a538a444ee9e2b57e2441a7c21");
+				.hexToBytes("0d0058208d3ef56d1b750a4351d68ac250a0e883790efc80a538a444ee9e2b57e2441a7c21");
 
 		Assert.assertArrayEquals(expectedMessage1, message1);
 		
@@ -147,7 +144,6 @@ public class MessageProcessorTest {
 	 * Test writing of message 1 with ciphersuite 2 and method 3.
 	 * 
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage1Ciphersuite2Method3() {
 		// First set up the session to use
@@ -179,7 +175,7 @@ public class MessageProcessorTest {
 		
 		// Self-produced (test vectors are still missing)
 		byte[] expectedMessage1 = Utils
-				.hexToBytes("400d025820475776f844979ad0b463c5a6a4343a663d17a3a80e38a81d3e3496f6061fd71621");
+				.hexToBytes("0d025820475776f844979ad0b463c5a6a4343a663d17a3a80e38a81d3e3496f6061fd71621");
 
 		Assert.assertArrayEquals(expectedMessage1, message1);
 		
@@ -190,7 +186,6 @@ public class MessageProcessorTest {
 	 * 
 	 * See: https://tools.ietf.org/html/draft-ietf-lake-edhoc-05#appendix-B.1.2
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage2B1() {
 
@@ -297,7 +292,7 @@ public class MessageProcessorTest {
 		session.setPeerConnectionId(connectionIdInitiator);
 		
 		// Store the EDHOC Message 1
-		byte[] message1 = Utils.hexToBytes("4001005820898ff79a02067a16ea1eccb90fa52246f5aa4dd6ec076bba0259d904b7ec8b0c2e");
+		byte[] message1 = Utils.hexToBytes("01005820898ff79a02067a16ea1eccb90fa52246f5aa4dd6ec076bba0259d904b7ec8b0c2e");
 		session.setMessage1(message1);
 		
 		
@@ -307,7 +302,7 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 		
 		byte[] expectedMessage2 = Utils
-				.hexToBytes("582071a3d599c21da18902a1aea810b2b6382ccd8d5f9bf0195281754c5ebcaf301e375850334097283a57820696782ba2be46c2ec2e02ecd55d88fe43084fedf97c9bb8122580e3f145c4632d5b677a25bf836a1d2209f15cba9ecfb267412e225983db18fe4f80b1be1d6508299cf88da9bbe9f3");
+				.hexToBytes("582071a3d599c21da18902a1aea810b2b6382ccd8d5f9bf0195281754c5ebcaf301e3758500ff2ac2d7e87ae340e50bbde9f70e8a77f86bf659f43b024a73ee97b6a2b9c5592fd835a15178b7c28af5474a9758148647d3d98a8731e164c9c70528107f40f21463ba811bf039719e7cffaa7f2f440");
 		
 		Assert.assertArrayEquals(expectedMessage2, message2);
 		
@@ -319,7 +314,6 @@ public class MessageProcessorTest {
 	 * 
 	 * See: https://tools.ietf.org/html/draft-ietf-lake-edhoc-05#appendix-B.2.2
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage2B2() {
 
@@ -381,7 +375,7 @@ public class MessageProcessorTest {
 		session.setPeerConnectionId(connectionIdInitiator);
 		
 		// Store the EDHOC Message 1
-		byte[] message1 = Utils.hexToBytes("400d0058208d3ef56d1b750a4351d68ac250a0e883790efc80a538a444ee9e2b57e2441a7c21");
+		byte[] message1 = Utils.hexToBytes("0d0058208d3ef56d1b750a4351d68ac250a0e883790efc80a538a444ee9e2b57e2441a7c21");
 		session.setMessage1(message1);
 		
 		
@@ -391,7 +385,7 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 		
 		byte[] expectedMessage2 = Utils
-				.hexToBytes("582052fba0bdc8d953dd86ce1ab2fd7c05a4658c7c30afdbfc3301047069451baf35374a7bbf6ba5f705fb8cc38e");
+				.hexToBytes("582052fba0bdc8d953dd86ce1ab2fd7c05a4658c7c30afdbfc3301047069451baf35374aa3f1bd5d028d19cf3c99");
 
 		Assert.assertArrayEquals(expectedMessage2, message2);
 		
@@ -401,7 +395,6 @@ public class MessageProcessorTest {
 	 * Test writing of message 2 with ciphersuite 2 and method 3.
 	 * 
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage2Ciphersuite2Method3() {
 
@@ -464,7 +457,7 @@ public class MessageProcessorTest {
 		session.setPeerConnectionId(connectionIdInitiator);
 		
 		// Store the EDHOC Message 1
-		byte[] message1 = Utils.hexToBytes("400d025820475776f844979ad0b463c5a6a4343a663d17a3a80e38a81d3e3496f6061fd71621");
+		byte[] message1 = Utils.hexToBytes("0d025820475776f844979ad0b463c5a6a4343a663d17a3a80e38a81d3e3496f6061fd71621");
 		session.setMessage1(message1);
 		
 		
@@ -475,7 +468,7 @@ public class MessageProcessorTest {
 		
 		// Self-produced (test vectors are still missing)
 		byte[] expectedMessage2 = Utils
-				.hexToBytes("582081df54b3756acfc8a1e9b08ba10de4e7e7dd934587a1ecdb21b92f8f22c3a38d374a6fb4eae89d984145dc05");
+				.hexToBytes("582081df54b3756acfc8a1e9b08ba10de4e7e7dd934587a1ecdb21b92f8f22c3a38d374adecc8c4895e398a6b9c8");
 
 		Assert.assertArrayEquals(expectedMessage2, message2);
 		
@@ -486,7 +479,6 @@ public class MessageProcessorTest {
 	 * 
 	 * See: https://tools.ietf.org/html/draft-ietf-lake-edhoc-05#appendix-B.1.3
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage3B1() {
 
@@ -536,8 +528,8 @@ public class MessageProcessorTest {
 
 		
 		/* Status from after receiving EDHOC Message 2 */
-		byte[] th2 = Utils.hexToBytes("655cab4d8cf4de31a8a154ca064e20c9046ec37bfc47499e58caab23a42cb840");
-		byte[] ciphertext2 = Utils.hexToBytes("334097283a57820696782ba2be46c2ec2e02ecd55d88fe43084fedf97c9bb8122580e3f145c4632d5b677a25bf836a1d2209f15cba9ecfb267412e225983db18fe4f80b1be1d6508299cf88da9bbe9f3");
+		byte[] th2 = Utils.hexToBytes("864e32b36a7b5f21f19e99f0c66d911e0ace9972d376d2c2c153c17f8e9629ff");
+		byte[] ciphertext2 = Utils.hexToBytes("0ff2ac2d7e87ae340e50bbde9f70e8a77f86bf659f43b024a73ee97b6a2b9c5592fd835a15178b7c28af5474a9758148647d3d98a8731e164c9c70528107f40f21463ba811bf039719e7cffaa7f2f440");
 		byte[] prk3e2m = Utils.hexToBytes("ec6292a067f137fc7f59629d226fbfc4e0688949f662a97fd82fbeb79971394a");
 		
 		
@@ -573,7 +565,7 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 		
 		byte[] expectedMessage3 = Utils
-				.hexToBytes("3758581c6682d608d6adce9f82a3eb13438dc7db43aa7ef0b1acc362fe6f74d8c39b31defb9d6e9e3afb24a47f9b8be72122637d922a43a1d7770b8719be1a517b75bef965812c10f7d7cdc663bdaa09c5c9fa342feb07a3c245fa");
+				.hexToBytes("375858f5f6debd8214051cd583c84096c4801debf35b15363dd16ebd8530dfdcfb34fcd2eb6cad1dac66a479fb38deaaf1d30a7e6817a22ab04f3d5b1e972a0d13ea86c66b60514c9657ea89c57b0401edc5aa8bbcab813cc5d6e7");
 
 		Assert.assertArrayEquals(expectedMessage3, message3);
 		
@@ -586,8 +578,8 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
         
 		// From version -02 of the draft
-		byte[] expectedMasterSecret = Utils.hexToBytes("318a986d6d3ad6bb41e9660b635bc722");
-		byte[] expectedMasterSalt = Utils.hexToBytes("64c21f5dd0837d3b");
+		byte[] expectedMasterSecret = Utils.hexToBytes("96aa88ce865eba1ffaf38964132cc442");
+		byte[] expectedMasterSalt = Utils.hexToBytes("5ec3ee417cfbbae9");
         
         Assert.assertArrayEquals(expectedMasterSecret, masterSecret);
         Assert.assertArrayEquals(expectedMasterSalt, masterSalt);
@@ -603,7 +595,6 @@ public class MessageProcessorTest {
 	 * 
 	 * See: https://tools.ietf.org/html/draft-ietf-lake-edhoc-05#appendix-B.2.3
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage3B2() {
 
@@ -650,8 +641,8 @@ public class MessageProcessorTest {
 
 		
 		/* Status from after receiving EDHOC Message 2 */
-		byte[] th2 = Utils.hexToBytes("a51c76463e8ae58fd3b8dc5ede1e27143cc92d223eacd9e5ff6e3fac876658a5");
-		byte[] ciphertext2 = Utils.hexToBytes("7bbf6ba5f705fb8cc38e");
+		byte[] th2 = Utils.hexToBytes("decfd64a3667640a0233b04aa8aa91f68956b8a536d0cf8c73a6e8a7c3621e26");
+		byte[] ciphertext2 = Utils.hexToBytes("a3f1bd5d028d19cf3c99");
 		byte[] prk3e2m = Utils.hexToBytes("75077c691e35012d48bc24c84f2bab89f52fac03fedd813e438c93b10b399307");
 		
 		
@@ -687,7 +678,7 @@ public class MessageProcessorTest {
 
 		// Compare with the expected value from the test vectors
 		
-		byte[] expectedMessage3 = Utils.hexToBytes("3752a2f92146c68923afb693abf3d84dc1f66921");
+		byte[] expectedMessage3 = Utils.hexToBytes("3752d5535f3147e85f1cfacd9e78abf9e0a81bbf");
 
 		Assert.assertArrayEquals(expectedMessage3, message3);
 		
@@ -700,8 +691,8 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 		
 		// From version -02 of the draft
-		byte[] expectedMasterSecret = Utils.hexToBytes("2426df745ceaf4793f95e1546203302b");
-		byte[] expectedMasterSalt = Utils.hexToBytes("528f2637600d3d41");
+		byte[] expectedMasterSecret = Utils.hexToBytes("c34a506d0ebfbd17030486135f9cb350");
+		byte[] expectedMasterSalt = Utils.hexToBytes("c224349d9b34ca8c");
         
         Assert.assertArrayEquals(expectedMasterSecret, masterSecret);
         Assert.assertArrayEquals(expectedMasterSalt, masterSalt);
@@ -715,7 +706,6 @@ public class MessageProcessorTest {
 	 * Test writing of message 3 with ciphersuite 2 and method 3.
 	 * 
 	 */
-	@Ignore
 	@Test
 	public void testWriteMessage3Ciphersuite2Method3() {
 
@@ -764,8 +754,8 @@ public class MessageProcessorTest {
 
 		
 		/* Status from after receiving EDHOC Message 2 */
-		byte[] th2 = Utils.hexToBytes("b98f08de78235868ba2b3039a9df73e4b8e90a228b1bd5e7dbe14785484265c9");
-		byte[] ciphertext2 = Utils.hexToBytes("6fb4eae89d984145dc05");
+		byte[] th2 = Utils.hexToBytes("d5672ca43b8a3e8ddab270174a4bb101772c5c206c12f435565c4748781152b5");
+		byte[] ciphertext2 = Utils.hexToBytes("decc8c4895e398a6b9c8");
 		byte[] prk3e2m = Utils.hexToBytes("80f79d96d715f22481ee8e906aa7f4c1aaa25207437d9a26baee32e393ed72be");
 		
 		
@@ -801,7 +791,7 @@ public class MessageProcessorTest {
 		// Compare with the expected value from the test vectors
 		
 		// Self-produced (test vectors are still missing)
-		byte[] expectedMessage3 = Utils.hexToBytes("3752c6530c2e0a293c8c224a142ccf959c40af5b");
+		byte[] expectedMessage3 = Utils.hexToBytes("37521055d4dada6efae005eb5e42ad37cc33c168");
 		
 		Assert.assertArrayEquals(expectedMessage3, message3);
 		
@@ -812,8 +802,8 @@ public class MessageProcessorTest {
         byte[] masterSalt = EdhocSession.getMasterSaltOSCORE(session);
        
 		// Self-produced (test vectors are still missing)
-		byte[] expectedMasterSecret = Utils.hexToBytes("37478cdb0eb91bdcf88cb706563b4d3e");
-		byte[] expectedMasterSalt = Utils.hexToBytes("ef81ea17105e7c67");
+		byte[] expectedMasterSecret = Utils.hexToBytes("d03b2f9c614f80be341703aad3116084");
+		byte[] expectedMasterSalt = Utils.hexToBytes("342ba2af96607d2d");
         
         Assert.assertArrayEquals(expectedMasterSecret, masterSecret);
         Assert.assertArrayEquals(expectedMasterSalt, masterSalt);
