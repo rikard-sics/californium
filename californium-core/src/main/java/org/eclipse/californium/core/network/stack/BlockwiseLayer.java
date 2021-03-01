aa476608e7a972c055962dc005d5e4498ebf7437
rikard-sics <rikard.hoglund@ri.se>
2021-03-01 19:16:30 +0100

    General updates for BW multicast

--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/BlockwiseLayer.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/BlockwiseLayer.java
@@ -1686,3 +1686,4 @@ public class BlockwiseLayer extends AbstractLayer {
 		return block1Transfers.size() == 0 && block2Transfers.size() == 0;
 	}
 }
+
--- californium-core/src/main/java/org/eclipse/californium/core/network/stack/KeyUri.java
+++ californium-core/src/main/java/org/eclipse/californium/core/network/stack/KeyUri.java
@@ -159,3 +159,4 @@ public final class KeyUri {
 	}
 
 }
+
