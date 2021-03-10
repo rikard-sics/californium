a4d1ca4a4895afeec17028ea3b354753e4b1d4c1
marco-tiloca-sics <marco.tiloca@ri.se>
2021-03-10 19:23:09 +0100

    Added EDHOC option

--- californium-core/src/main/java/org/eclipse/californium/core/coap/OptionSet.java
+++ californium-core/src/main/java/org/eclipse/californium/core/coap/OptionSet.java
@@ -84,6 +84,7 @@ public final class OptionSet {
 	private Integer      observe;
 	private byte[]       oscore;
 	private NoResponseOption no_response;
+	private boolean      edhoc; // EDHOC
 
 	// Arbitrary options
 	private List<Option> others;
@@ -116,6 +117,7 @@ public final class OptionSet {
 		observe             = null;
 		oscore              = null;
 		no_response         = null;
+		edhoc               = false; // EDHOC
 
 		others              = null; // new LinkedList<>();
 	}
@@ -154,6 +156,9 @@ public final class OptionSet {
 			oscore          = origin.oscore.clone();
 		}
 		no_response         = origin.no_response;
+		
+		edhoc               = origin.edhoc;  // EDHOC
+
 		others              = copyList(origin.others);
 	}
 
@@ -188,6 +193,7 @@ public final class OptionSet {
 		observe = null;
 		oscore = null;
 		no_response = null;
+		edhoc = false; // EDHOC
 		if (others != null)
 			others.clear();
 	}
@@ -1435,6 +1441,28 @@ public final class OptionSet {
 		oscore = null;
 		return this;
 	}
+	
+	// EDHOC
+	/**
+	 * Checks if the EDHOC option is present.
+	 * 
+	 * @return {@code true}, if present
+	 */
+	public boolean hasEdhoc() {
+		return edhoc;
+	}
+
+	// EDHOC
+	/**
+	 * Sets or unsets the EDHOC option.
+	 * 
+	 * @param present the presence of the option
+	 * @return this OptionSet for a fluent API.
+	 */
+	public OptionSet setEdhoc(boolean present) {
+		edhoc = present;
+		return this;
+	}
 
 	/**
 	 * Gets the NoResponse option.
@@ -1588,6 +1616,10 @@ public final class OptionSet {
 		if (hasNoResponse())
 			options.add(getNoResponse().toOption());
 
+		// EDHOC
+		if (hasEdhoc())
+			options.add(new Option(OptionNumberRegistry.EDHOC));
+		
 		if (others != null)
 			options.addAll(others);
 
@@ -1700,6 +1732,12 @@ public final class OptionSet {
 		case OptionNumberRegistry.NO_RESPONSE:
 			setNoResponse(option.getIntegerValue());
 			break;
+			
+		// EDHOC
+		case OptionNumberRegistry.EDHOC:
+			setEdhoc(true);
+			break;
+
 		default:
 			getOthersInternal().add(option);
 		}
