1696e90aca4d1f69aa4b604f9c564f25f5a73189
rikard-sics <rikard.hoglund@ri.se>
2021-09-21 17:39:15 +0200

    UCCS with keys in Group Sender/Receiver now use deterministic CBOR enc.
    According to https://datatracker.ietf.org/doc/html/rfc8949#section-4.2.1

--- cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupOSCORESender.java
+++ cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupOSCORESender.java
@@ -46,8 +46,7 @@ import net.i2p.crypto.eddsa.EdDSASecurityProvider;
 import org.eclipse.californium.core.network.config.NetworkConfigDefaultHandler;
 
 /**
- * Test sender configured to support multicast requests.
- * Rebased.
+ * Test sender configured to support multicast requests. Rebased.
  */
 public class GroupOSCORESender {
 
@@ -126,10 +125,12 @@ public class GroupOSCORESender {
 
 	private static final int REPLAY_WINDOW = 32;
 
-	private final static String gm_public_key_string = "pQF4GmNvYXBzOi8vbXlzaXRlLmV4YW1wbGUuY29tAmxncm91cG1hbmFnZXIDeBpjb2FwczovL2RvbWFpbi5leGFtcGxlLm9yZwQaq5sVTwihAaQDJwEBIAYhWCDN4+/TvD+ZycnuIQQVxsulUGG1BG6WO4pYyRQ6YRZkcg==";
+	private final static byte[] gm_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781A636F6170733A2F2F6D79736974652E6578616D706C652E636F6D026C67726F75706D616E6167657203781A636F6170733A2F2F646F6D61696E2E6578616D706C652E6F7267041AAB9B154F08A101A4010103272006215820CDE3EFD3BC3F99C9C9EE210415C6CBA55061B5046E963B8A58C9143A61166472");
 
 	private final static byte[] sid = new byte[] { 0x25 };
-	private final static String sid_private_key_string = "pQF4G2NvYXBzOi8vdGVzdGVyMS5leGFtcGxlLmNvbQJmbXluYW1lA3gaY29hcHM6Ly9oZWxsbzEuZXhhbXBsZS5vcmcEGnAAS08IoQGkAycBASAGIVggBp6RK4OWOsxZQbY1RoZ97BBuW5BR8u4U87xcyWGs1Do=";
+	private final static byte[] sid_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781B636F6170733A2F2F746573746572312E6578616D706C652E636F6D02666D796E616D6503781A636F6170733A2F2F68656C6C6F312E6578616D706C652E6F7267041A70004B4F08A101A4010103272006215820069E912B83963ACC5941B63546867DEC106E5B9051F2EE14F3BC5CC961ACD43A");
 	private static MultiKey sid_private_key;
 	private static byte[] sid_private_key_bytes = new byte[] { (byte) 0x64, (byte) 0x71, (byte) 0x4D, (byte) 0x41,
 			(byte) 0xA2, (byte) 0x40, (byte) 0xB6, (byte) 0x1D, (byte) 0x8D, (byte) 0x82, (byte) 0x35, (byte) 0x02,
@@ -138,11 +139,13 @@ public class GroupOSCORESender {
 			(byte) 0xCC, (byte) 0x73, (byte) 0x52, (byte) 0x39 };
 
 	private final static byte[] rid1 = new byte[] { 0x52 }; // Recipient 1
-	private final static String rid1_public_key_string = "pQF4GmNvYXBzOi8vc2VydmVyLmV4YW1wbGUuY29tAmZzZW5kZXIDeBpjb2FwczovL2NsaWVudC5leGFtcGxlLm9yZwQacABLTwihAaQDJwEBIAYhWCB37DWMHTROQe4Oh7g4PSOiCZrNOb35ic5FtS6IdGM4mw==";
+	private static byte[] rid1_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781A636F6170733A2F2F7365727665722E6578616D706C652E636F6D026673656E64657203781A636F6170733A2F2F636C69656E742E6578616D706C652E6F7267041A70004B4F08A101A401010327200621582077EC358C1D344E41EE0E87B8383D23A2099ACD39BDF989CE45B52E887463389B");
 	private static MultiKey rid1_public_key;
 
 	private final static byte[] rid2 = new byte[] { 0x77 }; // Recipient 2
-	private final static String rid2_public_key_string = "pQF4GmNvYXBzOi8vc2VydmVyLmV4YW1wbGUuY29tAmZzZW5kZXIDeBpjb2FwczovL2NsaWVudC5leGFtcGxlLm9yZwQacABLTwihAaQDJwEBIAYhWCAQW4xqjIgBm/DDVFkpNBMLqoAHOZzCrDvoRYhGE9W6Lg==";
+	private final static byte[] rid2_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781A636F6170733A2F2F7365727665722E6578616D706C652E636F6D026673656E64657203781A636F6170733A2F2F636C69656E742E6578616D706C652E6F7267041A70004B4F08A101A4010103272006215820105B8C6A8C88019BF0C354592934130BAA8007399CC2AC3BE845884613D5BA2E");
 	private static MultiKey rid2_public_key;
 
 	private final static byte[] rid0 = new byte[] { (byte) 0xCC }; // Dummy
@@ -169,15 +172,14 @@ public class GroupOSCORESender {
 		// InstallCryptoProviders.generateCounterSignKey();
 
 		// Add private & public keys for sender & receiver(s)
-		sid_private_key = new MultiKey(DatatypeConverter.parseBase64Binary(sid_private_key_string),
-				sid_private_key_bytes);
-		rid1_public_key = new MultiKey(DatatypeConverter.parseBase64Binary(rid1_public_key_string));
-		rid2_public_key = new MultiKey(DatatypeConverter.parseBase64Binary(rid2_public_key_string));
+		sid_private_key = new MultiKey(sid_public_key_bytes, sid_private_key_bytes);
+		rid1_public_key = new MultiKey(rid1_public_key_bytes);
+		rid2_public_key = new MultiKey(rid2_public_key_bytes);
 
 		// If OSCORE is being used set the context information
 		if (useOSCORE) {
 
-			byte[] gmPublicKey = DatatypeConverter.parseBase64Binary(gm_public_key_string);
+			byte[] gmPublicKey = gm_public_key_bytes;
 			GroupCtx commonCtx = new GroupCtx(master_secret, master_salt, alg, kdf, group_identifier, algCountersign,
 					gmPublicKey);
 
