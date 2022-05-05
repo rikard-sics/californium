22eef33a1b336587b8af3b423b7e258789e5307b
rhogl <rikard.hoglund@ri.se>
2022-05-05 17:27:40 +0200

    Rebase on latest Californium (at commit e80b2bb) and fix code
    Also reset the hello world client and server

--- cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupOSCoreServerClientTest.java
+++ cf-oscore/src/test/java/org/eclipse/californium/oscore/group/GroupOSCoreServerClientTest.java
@@ -65,6 +65,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
