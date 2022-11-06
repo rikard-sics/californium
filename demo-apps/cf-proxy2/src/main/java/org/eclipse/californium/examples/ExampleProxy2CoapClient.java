dfb167eb3e58831cabe4c1572addc538f06880b8
Rikard Höglund <rikard.hoglund@ri.se>
2022-11-06 15:03:28 +0100

    Adding Group OSCORE to client/server communicating via proxy

--- demo-apps/cf-proxy2/src/main/java/org/eclipse/californium/examples/ExampleProxy2CoapClient.java
+++ demo-apps/cf-proxy2/src/main/java/org/eclipse/californium/examples/ExampleProxy2CoapClient.java
@@ -19,6 +19,8 @@ package org.eclipse.californium.examples;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.Provider;
+import java.security.Security;
 
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapHandler;
@@ -30,12 +32,17 @@ import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.elements.config.TcpConfig;
 import org.eclipse.californium.elements.config.UdpConfig;
 import org.eclipse.californium.elements.exception.ConnectorException;
+import org.eclipse.californium.elements.util.Bytes;
 import org.eclipse.californium.examples.util.CoapResponsePrinter;
 import org.eclipse.californium.oscore.HashMapCtxDB;
 import org.eclipse.californium.oscore.OSCoreCoapStackFactory;
 import org.eclipse.californium.oscore.OSCoreCtx;
 import org.eclipse.californium.oscore.OSException;
+import org.eclipse.californium.oscore.group.GroupCtx;
+import org.eclipse.californium.oscore.group.MultiKey;
 import org.eclipse.californium.proxy2.resources.ResponseForwardingOption;
+
+import net.i2p.crypto.eddsa.EdDSASecurityProvider;
 
 /**
  * Class ExampleProxyCoapClient.
@@ -81,25 +88,67 @@ import org.eclipse.californium.proxy2.resources.ResponseForwardingOption;
  */
 public class ExampleProxy2CoapClient {
 
+	/* --- OSCORE Security Context information (sender) --- */
 	private final static HashMapCtxDB db = new HashMapCtxDB();
-	// private final static String uriLocal = "coap://localhost";
-	// private final static String hello1 = "/hello/1";
 	private final static AlgorithmID alg = AlgorithmID.AES_CCM_16_64_128;
 	private final static AlgorithmID kdf = AlgorithmID.HKDF_HMAC_SHA_256;
-	private final static int MAX_UNFRAGMENTED_SIZE = 4096;
 
-	/**
-	 * Time to wait for replies to the multicast request
-	 */
-	private static final int HANDLER_TIMEOUT = 5000;
+	// Group OSCORE specific values for the countersignature (EdDSA)
+	private final static AlgorithmID algCountersign = AlgorithmID.EDDSA;
+
+	// Encryption algorithm for when using signatures
+	private final static AlgorithmID algSignEnc = AlgorithmID.AES_CCM_16_64_128;
+
+	// Algorithm for key agreement
+	private final static AlgorithmID algKeyAgreement = AlgorithmID.ECDH_SS_HKDF_256;
 
 	// test vector OSCORE draft Appendix C.1.1
 	private final static byte[] master_secret = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B,
 			0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
 	private final static byte[] master_salt = { (byte) 0x9e, (byte) 0x7c, (byte) 0xa9, (byte) 0x22, (byte) 0x23,
 			(byte) 0x78, (byte) 0x63, (byte) 0x40 };
-	private final static byte[] sid = new byte[0];
-	private final static byte[] rid = new byte[] { 0x01 };
+
+	private static final int REPLAY_WINDOW = 32;
+
+	private final static byte[] gm_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781A636F6170733A2F2F6D79736974652E6578616D706C652E636F6D026C67726F75706D616E6167657203781A636F6170733A2F2F646F6D61696E2E6578616D706C652E6F7267041AAB9B154F08A101A4010103272006215820CDE3EFD3BC3F99C9C9EE210415C6CBA55061B5046E963B8A58C9143A61166472");
+
+	private final static byte[] sid = new byte[] { 0x25 };
+	private final static byte[] sid_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781B636F6170733A2F2F746573746572312E6578616D706C652E636F6D02666D796E616D6503781A636F6170733A2F2F68656C6C6F312E6578616D706C652E6F7267041A70004B4F08A101A4010103272006215820069E912B83963ACC5941B63546867DEC106E5B9051F2EE14F3BC5CC961ACD43A");
+	private static MultiKey sid_private_key;
+	private static byte[] sid_private_key_bytes = new byte[] { (byte) 0x64, (byte) 0x71, (byte) 0x4D, (byte) 0x41,
+			(byte) 0xA2, (byte) 0x40, (byte) 0xB6, (byte) 0x1D, (byte) 0x8D, (byte) 0x82, (byte) 0x35, (byte) 0x02,
+			(byte) 0x71, (byte) 0x7A, (byte) 0xB0, (byte) 0x88, (byte) 0xC9, (byte) 0xF4, (byte) 0xAF, (byte) 0x6F,
+			(byte) 0xC9, (byte) 0x84, (byte) 0x45, (byte) 0x53, (byte) 0xE4, (byte) 0xAD, (byte) 0x4C, (byte) 0x42,
+			(byte) 0xCC, (byte) 0x73, (byte) 0x52, (byte) 0x39 };
+
+	private final static byte[] rid1 = new byte[] { 0x52 }; // Recipient 1
+	private static byte[] rid1_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781A636F6170733A2F2F7365727665722E6578616D706C652E636F6D026673656E64657203781A636F6170733A2F2F636C69656E742E6578616D706C652E6F7267041A70004B4F08A101A401010327200621582077EC358C1D344E41EE0E87B8383D23A2099ACD39BDF989CE45B52E887463389B");
+	private static MultiKey rid1_public_key;
+
+	private final static byte[] rid2 = new byte[] { 0x77 }; // Recipient 2
+	private final static byte[] rid2_public_key_bytes = net.i2p.crypto.eddsa.Utils.hexToBytes(
+			"A501781A636F6170733A2F2F7365727665722E6578616D706C652E636F6D026673656E64657203781A636F6170733A2F2F636C69656E742E6578616D706C652E6F7267041A70004B4F08A101A4010103272006215820105B8C6A8C88019BF0C354592934130BAA8007399CC2AC3BE845884613D5BA2E");
+	private static MultiKey rid2_public_key;
+
+	private final static byte[] rid0 = new byte[] { (byte) 0xCC }; // Dummy
+
+	private final static byte[] group_identifier = new byte[] { 0x44, 0x61, 0x6c }; // GID
+
+	private final static int MAX_UNFRAGMENTED_SIZE = 4096;
+
+	static String serverResourceUri = "coap://224.0.1.187:5685/coap-target";
+
+	static boolean USE_GROUP_OSCORE = true;
+
+	/* --- OSCORE Security Context information --- */
+
+	/**
+	 * Time to wait for replies to the multicast request
+	 */
+	private static final int HANDLER_TIMEOUT = 5000;
 
 	static {
 		CoapConfig.register();
@@ -125,19 +174,55 @@ public class ExampleProxy2CoapClient {
 		}
 	}
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, OSException, URISyntaxException {
 
-		URI proxyUri = null;
-		try {
-			OSCoreCtx ctx = new OSCoreCtx(master_secret, true, alg, sid, rid, kdf, 32, master_salt, null,
-					MAX_UNFRAGMENTED_SIZE);
-			db.addContext("coap://localhost", ctx);
+		// Install cryptographic providers
+		Provider EdDSA = new EdDSASecurityProvider();
+		Security.insertProviderAt(EdDSA, 1);
+		// InstallCryptoProviders.generateCounterSignKey();
+
+		// Add private & public keys for sender & receiver(s)
+		sid_private_key = new MultiKey(sid_public_key_bytes, sid_private_key_bytes);
+		rid1_public_key = new MultiKey(rid1_public_key_bytes);
+		rid2_public_key = new MultiKey(rid2_public_key_bytes);
+
+		// If OSCORE is being used set the context information
+		if (USE_GROUP_OSCORE) {
+
+			byte[] gmPublicKey = gm_public_key_bytes;
+			GroupCtx commonCtx = new GroupCtx(master_secret, master_salt, alg, kdf, group_identifier, algCountersign,
+					algSignEnc, algKeyAgreement, gmPublicKey);
+
+			commonCtx.addSenderCtxCcs(sid, sid_private_key);
+
+			commonCtx.addRecipientCtxCcs(rid0, REPLAY_WINDOW, null);
+			commonCtx.addRecipientCtxCcs(rid1, REPLAY_WINDOW, rid1_public_key);
+			commonCtx.addRecipientCtxCcs(rid2, REPLAY_WINDOW, rid2_public_key);
+
+			commonCtx.setResponsesIncludePartialIV(true);
+			commonCtx.setResponsesIncludePartialIV(true);
+
+			db.addContext(serverResourceUri, commonCtx);
+
 			OSCoreCoapStackFactory.useAsDefault(db);
-			proxyUri = new URI("coap", "localhost", null, null);
-		} catch (OSException | URISyntaxException e) {
-			System.err.println("Failed to add OSCORE context: " + e);
-			e.printStackTrace();
 		}
+
+		URI proxyUri = new URI("coap", "localhost", null, null);
+		;
+
+		/*
+		 * try {
+		 * 
+		 * OSCoreCtx ctx = new OSCoreCtx(master_secret, true, alg, sid, rid,
+		 * kdf, 32, master_salt, null, MAX_UNFRAGMENTED_SIZE);
+		 * 
+		 * db.addContext("coap://localhost", ctx);
+		 * 
+		 * OSCoreCoapStackFactory.useAsDefault(db); proxyUri = new URI("coap",
+		 * "localhost", null, null); } catch (OSException | URISyntaxException
+		 * e) { System.err.println("Failed to add OSCORE context: " + e);
+		 * e.printStackTrace(); }
+		 */
 
 		CoapClient client = new CoapClient();
 		// // deprecated proxy request - use CoAP and Proxy URI together
@@ -342,8 +427,11 @@ public class ExampleProxy2CoapClient {
 		request = Request.newGet();
 		// request.setDestinationContext(proxy); // Doesn't work for OSCORE
 		request.setURI(proxyUri.toString());
-		request.getOptions().setProxyUri("coap://224.0.1.187:5685/coap-target");
+		request.getOptions().setProxyUri(serverResourceUri);
 		request.getOptions().setObserve(0);
+		if (USE_GROUP_OSCORE) {
+			request.getOptions().setOscore(Bytes.EMPTY);
+		}
 		System.out.println("Proxy-URI: " + request.getOptions().getProxyUri());
 
 		request.setMultiResponse(true);
