1c1d05dd488ef624039bf4547b5e2edf9dd58f10
Rikard Höglund <rikard.hoglund@ri.se>
2022-11-06 17:31:34 +0100

    Fix for Group OSCORE on the server going via proxy

--- demo-apps/cf-proxy2/src/main/java/org/eclipse/californium/examples/ExampleCoapServer.java
+++ demo-apps/cf-proxy2/src/main/java/org/eclipse/californium/examples/ExampleCoapServer.java
@@ -59,6 +59,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.i2p.crypto.eddsa.EdDSASecurityProvider;
+import net.i2p.crypto.eddsa.Utils;
 
 /**
  * Example CoAP server for proxy demonstration.
@@ -206,7 +207,10 @@ public class ExampleCoapServer {
 				System.out.println("=== End Receiving incoming request ===");
 
 				String payload = "Hi! I am the " + scheme + " server on port " + port + ". Request "
-						+ counter.incrementAndGet() + " with ID: " + serverId + ".";
+						+ counter.incrementAndGet() + " with ID: " + serverId;
+				if (USE_GROUP_OSCORE) {
+					payload += " and Group OSCORE SID: " + Utils.bytesToHex(sid) + ".";
+				}
 				exchange.setMaxAge(15);
 				int hash = payload.hashCode();
 				DatagramWriter etag = new DatagramWriter(4);
@@ -314,11 +318,11 @@ public class ExampleCoapServer {
 
 		Configuration config = init();
 		int port;
-		if (arg.length > 0) {
-			port = Integer.parseInt(arg[0]);
-		} else {
+		// if (arg.length > 0) {
+		// port = Integer.parseInt(arg[0]);
+		// } else {
 			port = config.get(CoapConfig.COAP_PORT);
-		}
+		// }
 		new ExampleCoapServer(config, port);
 	}
 
