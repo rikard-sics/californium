0a0bce037b1157e6e1e960b3fc824c0e49bfa981
rhogl <rikard.hoglund@ri.se>
2023-05-17 07:42:11 +0200

    WIP for adding AES-CCM 256 bit key, 64/128 bit tag, 7/13 byte nonce
    Signed-off-by: Rikard Höglund <rikard.hoglund@ri.se>

--- cf-oscore/src/test/java/org/eclipse/californium/oscore/group/ChaChaPolyTester.java
+++ cf-oscore/src/test/java/org/eclipse/californium/oscore/group/ChaChaPolyTester.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2023 RISE SICS and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * 
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * This test class is based on org.eclipse.californium.core.test.SmallServerClientTest
+ * 
+ * Contributors: 
+ *    Rikard Höglund (RISE SICS)
+ ******************************************************************************/
 package org.eclipse.californium.oscore.group;
 
 import org.bouncycastle.crypto.modes.ChaCha20Poly1305;
