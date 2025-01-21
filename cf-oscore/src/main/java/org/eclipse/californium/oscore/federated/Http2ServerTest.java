50f04eaccdc83ae7301d810a4e7690bee604de2f
Rikard Höglund <rikard.hoglund@ri.se>
2025-01-21 21:15:53 +0100

    WIP HTTP2 client server more

--- cf-oscore/src/main/java/org/eclipse/californium/oscore/federated/Http2ServerTest.java
+++ cf-oscore/src/main/java/org/eclipse/californium/oscore/federated/Http2ServerTest.java
@@ -1,6 +1,11 @@
 package org.eclipse.californium.oscore.federated;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
 import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConfiguration;
@@ -15,6 +20,7 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+
 
 public class Http2ServerTest {
 
@@ -54,18 +60,44 @@ public class Http2ServerTest {
 
 		server.addConnector(connector);
 
-
 		Handler myHandler = new Handler.Abstract()
 		{
 
-			@Override
-			public boolean handle(Request request, Response response, Callback callback) throws Exception {
+			public boolean handleOld(Request request, Response response, Callback callback) throws Exception {
 				// TODO Auto-generated method stub
 				System.out.println("Request received");
 				callback.succeeded();
 
+				ByteBuffer res = ByteBuffer.wrap(new byte[] { 0x61, 0x73, 0x64 });
+				response.write(true, res, callback);
 				return true;
 			}
+
+			@Override
+			public boolean handle(Request request, Response response, Callback callback) {
+				// Set the response status code.
+				response.setStatus(HttpStatus.OK_200);
+				// Set the response headers.
+				response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/plain");
+
+				String hello = "HELLO";
+
+				// Commit the response with a "flush" write.
+				Callback.Completable.with(flush -> response.write(false, null, flush))
+						// When the flush is finished, send the content and
+						// complete the callback.
+						.whenComplete((ignored, failure) -> {
+							if (failure == null)
+								response.write(true, null, callback);
+							else
+								callback.failed(failure);
+						});
+
+				// Return true because the callback will eventually be
+				// completed.
+				return true;
+			}
+
 		};
 
 		server.setHandler(myHandler);
