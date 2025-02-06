7adb45990173860726d0dc206fa1a823f7c7d75b
rikard-sics <rikard.hoglund@ri.se>
2025-02-06 21:03:42 +0100

    Refinements after interop

--- cf-oscore/src/main/java/org/eclipse/californium/oscore/Encryptor.java
+++ cf-oscore/src/main/java/org/eclipse/californium/oscore/Encryptor.java
@@ -93,6 +93,8 @@ public abstract class Encryptor {
 		groupModeMessage = ctx.isGroupContext() && !pairwiseResponse && !pairwiseRequest;
 		int nonceLength = ctx.getIVLength();
 		byte[] commonIV = ctx.getCommonIV();
+
+		// Set appropriate nonce length
 		if (ctx.isGroupContext() && groupModeMessage) {
 			int algGroupEncIvLen = EncryptCommon.getIvLength(((GroupSenderCtx) ctx).getAlgGroupEnc());
 			nonceLength = algGroupEncIvLen;
@@ -154,11 +156,9 @@ public abstract class Encryptor {
 			}
 
 			System.out.println("Encrypting outgoing " + message.getClass().getSimpleName());
-			System.out.println("Key " + Utils.toHexString(ctx.getSenderKey()));
 			System.out.println("Plaintext " + Utils.toHexString(enc.GetContent()));
 			System.out.println("PartialIV " + Utils.toHexString(partialIV));
 			System.out.println("Nonce " + Utils.toHexString(nonce));
-			System.out.println("AAD " + Utils.toHexString(aad));
 			System.out.println("Common IV " + Utils.toHexString(ctx.getCommonIV()));
 
 			// Handle Group OSCORE messages
@@ -189,6 +189,10 @@ public abstract class Encryptor {
 				}
 
 			}
+
+			System.out.println("AAD " + Utils.toHexString(aad));
+			System.out.println("Sender Key " + Utils.toHexString(ctx.getSenderKey()));
+			System.out.println("Key used " + Utils.toHexString(key));
 
 			// Warning: Using algos without integrity in pairwise mode
 			if (encryptionAlg.getTagSize() == 0 && !groupModeMessage) {
